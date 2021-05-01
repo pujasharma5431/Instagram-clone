2d10cd76292931dd8f8c4c0f1938248f668d00bb
arjun-sudo <arjungautam5431@gmail.com>
2021-05-02 00:19:33 +0545

    main page layout

--- instagram-frontend/src/Components/LoginPage.js
+++ instagram-frontend/src/Components/LoginPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './LoginPage.css'
 import Grid from '@material-ui/core/Grid';
 import insta_image from '../images/9364675fb26a.svg'
 import insta_logo from '../images/logoinsta.png'
@@ -6,29 +7,32 @@ import insta_logo from '../images/logoinsta.png'
 class LoginPage extends Component {
     constructor(props) {
         super(props);
-        this.state={ }
+        this.state = {}
     }
+
     render() {
         return (
             <div>
-               <Grid container>
-                   <Grid item xs={3}>
-                       Grid 1
-                   </Grid>
-                   <Grid item xs={6} className={"loginpage_main"}>
-                       <div>
-                           <div>
-                               <img src={insta_image} width={"554px"}/>
-                           </div>
-                           <div>
-                               <img src={insta_logo}/>
-                           </div>
-                       </div>
-                   </Grid>
-                   <Grid item xs={3}>
-                       Grid 3
-                   </Grid>
-               </Grid>
+                <Grid container>
+                    <Grid item xs={3}>
+
+                    </Grid>
+                    <Grid item xs={6} >
+                        <div className={"loginpage_main"}>
+                            <div>
+                                <img src={insta_image} width={"454px"}/>
+                            </div>
+                            <div>
+                                <div className={"loginpage_rightcomponent"}>
+                                    <img className={"loginpage_logo"} src={insta_logo}/>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={3}>
+
+                    </Grid>
+                </Grid>
             </div>
         );
     }
